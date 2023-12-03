959df6d968f78ff8e551ce3f1dab022bc1c078d8
geswanel <geswanel@gmail.com>
2023-12-04 00:23:03 +0700

    Created model for regular tasks and registered in admin

--- plannerweb/regulartasks/models.py
+++ plannerweb/regulartasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class RegularTask(models.Model):
+    FREQUENCY_RANGE_CHOICES = (
+        ('d', 'days'),
+        ('w', 'weeks'),
+        ('m', 'months'),
+        ('y', 'years')
+    )
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    last_execution = models.DateTimeField()
+    frequency_val = models.PositiveIntegerField()
+    frequency_range = models.CharField(max_length=1, choices=FREQUENCY_RANGE_CHOICES)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user_id}: {self.title}"
+    
